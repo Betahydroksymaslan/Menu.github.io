db4c097c9a0b9bf37ec32e4590cb320361fc4560
Betahydroksymaslan <wieczork@gmail.com>
2020-08-03 22:14:13 +0200

    next change

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const navBar = document.querySelector('.nav__bar');
+const menu  = document.querySelector('.menu');
 
-navBar.addEventListener('click', () => {
+menu.addEventListener('click', () => {
     navBar.classList.toggle('nav__bar--active')
 })
